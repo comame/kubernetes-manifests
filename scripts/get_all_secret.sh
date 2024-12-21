219327665d368ea8f401a4f98d9d1e0ee0b8feba
comame <dev@comame.xyz>
2024-12-22 04:34:03 +0900

    スクリプトのディレクトリ構成

--- scripts/get_all_secret.sh
+++ scripts/get_all_secret.sh
@@ -4,6 +4,6 @@ DATE=$(date +%Y%m%d-%H%M%S)
 FILENAME="secrets-$DATE.yaml"
 
 kubectl get secret --all-namespaces -o=yaml > "secrets/$FILENAME"
-deno -A scripts/commit_scripts.deno.ts "$FILENAME"
+deno -A scripts/internal/commit_scripts.deno.ts "$FILENAME"
 
 git add "secrets-commit/$FILENAME"
