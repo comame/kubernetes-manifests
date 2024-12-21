2a418401b9a897f205a815975e8838c753f87107
comame <dev@comame.xyz>
2024-12-22 03:05:33 +0900

    不要なコメントが残ってた

--- helm/chart/cert-manager.tf
+++ helm/chart/cert-manager.tf
@@ -1,6 +1,3 @@
-# FIXME: terraform apply に成功しないので、手動デプロイ。いずれ ArgoCD に移行する
-# helm install cert-manager cert-manager --repo https://charts.jetstack.io --version v1.13.3 --namespace cert-manager --create-namespace --set crds.enabled=true
-
 resource "helm_release" "cert-manager" {
   name       = "cert-manager"
   repository = "https://charts.jetstack.io"
