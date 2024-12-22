e61fa50decec19ef9e2d8e7a13f069cb949d1a16
comame <dev@comame.xyz>
2024-12-22 16:00:24 +0900

    calico v.3.29.1 にアップデート

--- helm/chart/calico.tf
+++ helm/chart/calico.tf
@@ -1,8 +1,10 @@
+# アップグレード時はドキュメント参照のこと。CRD を更新する必要がある
+# https://docs.tigera.io/calico/latest/operations/upgrading/kubernetes-upgrade
 resource "helm_release" "calico" {
   name       = "calico"
   repository = "https://docs.tigera.io/calico/charts"
   chart      = "tigera-operator"
-  version    = "3.28.0"
+  version    = "3.29.1"
 
   namespace        = "tigera-operator"
   create_namespace = true
